da9f892d16c143dd5657688ee2fc8f306d87f43c
Angelo Marconi <112192381+Angelo2222@users.noreply.github.com>
2022-09-28 11:52:04 -0300

    Add files via upload

---
+++ SP1-Presentacion/SP1/frmLogin.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SP1
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrasenia = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.cmbModulo = new System.Windows.Forms.ComboBox();
+            this.lblModulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(314, 115);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(317, 170);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(61, 13);
+            this.lblContraseña.TabIndex = 1;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.SystemColors.Window;
+            this.txtUsuario.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtUsuario.Location = new System.Drawing.Point(416, 115);
+            this.txtUsuario.MaxLength = 10;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 2;
+            this.txtUsuario.TextChanged += new System.EventHandler(this.txtUsuario_TextChanged);
+            // 
+            // txtContrasenia
+            // 
+            this.txtContrasenia.Location = new System.Drawing.Point(416, 170);
+            this.txtContrasenia.MaxLength = 10;
+            this.txtContrasenia.Name = "txtContrasenia";
+            this.txtContrasenia.PasswordChar = '*';
+            this.txtContrasenia.Size = new System.Drawing.Size(100, 20);
+            this.txtContrasenia.TabIndex = 3;
+            this.txtContrasenia.TextChanged += new System.EventHandler(this.txtContrasenia_TextChanged);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(586, 115);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 4;
+            this.btnAceptar.Text = "&Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(586, 170);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // cmbModulo
+            // 
+            this.cmbModulo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbModulo.FormattingEnabled = true;
+            this.cmbModulo.Items.AddRange(new object[] {
+            "ADM → Administración",
+            "SIST → Sistemas",
+            "COM → Compras",
+            "VTA → Ventas"});
+            this.cmbModulo.Location = new System.Drawing.Point(416, 228);
+            this.cmbModulo.Name = "cmbModulo";
+            this.cmbModulo.Size = new System.Drawing.Size(100, 21);
+            this.cmbModulo.TabIndex = 6;
+            this.cmbModulo.Tag = "";
+            this.cmbModulo.SelectedIndexChanged += new System.EventHandler(this.cmbModulo_SelectedIndexChanged);
+            // 
+            // lblModulo
+            // 
+            this.lblModulo.AutoSize = true;
+            this.lblModulo.Location = new System.Drawing.Point(317, 231);
+            this.lblModulo.Name = "lblModulo";
+            this.lblModulo.Size = new System.Drawing.Size(42, 13);
+            this.lblModulo.TabIndex = 7;
+            this.lblModulo.Text = "Modulo";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblModulo);
+            this.Controls.Add(this.cmbModulo);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtContrasenia);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "frmLogin";
+            this.Text = "LOGIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContrasenia;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.ComboBox cmbModulo;
+        private System.Windows.Forms.Label lblModulo;
+    }
+}
+
---
+++ SP1-Presentacion/SP1/frmLogin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP1
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            int intentos = 0;
+            if (txtUsuario.Text == "Adm" && txtContrasenia.Text == "@1a" && (cmbModulo.Text == "ADM → Administración" || cmbModulo.Text == "COM → Compras" || cmbModulo.Text == "VTA → Ventas"))
+            {
+                this.Hide();
+                frmInicio frmInicio = new frmInicio();
+                frmInicio.ShowDialog();
+            }
+            else
+            {
+            if (txtUsuario.Text == "Jonh" && txtContrasenia.Text == "*2b" && cmbModulo.Text == "SIST → Sistemas")
+            {
+                    this.Hide();
+                    frmInicio frmInicio = new frmInicio();
+                    frmInicio.ShowDialog();
+            }
+            else
+            if (txtUsuario.Text == "Ceci" && txtContrasenia.Text == "@3c" && (cmbModulo.Text == "ADM → Administración" || cmbModulo.Text == "VTA → Ventas"))
+            {
+                    this.Hide();
+                    frmInicio frmInicio = new frmInicio();
+                    frmInicio.ShowDialog();
+            }
+            else
+            if (txtUsuario.Text == "God" && txtContrasenia.Text == "@4d" && (cmbModulo.Text == "ADM → Administración" || cmbModulo.Text == "COM → Compras" || cmbModulo.Text == "VTA → Ventas" || cmbModulo.Text == "SIST → Sistemas"))
+            {
+                    this.Hide();
+                    frmInicio frmInicio = new frmInicio();
+                    frmInicio.ShowDialog();
+            }
+            else
+            {
+                    MessageBox.Show("No so VO.");
+                        intentos++; 
+                    if (intentos == 2) 
+                    {
+                        this.Close();
+                    }
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            // evento Click del botón "btnCancelar"
+            
+            {
+                this.Close(); // cierra el formulario
+            }
+
+        }
+
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbModulo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void txtContrasenia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
